736b0e939e859ddffff1d1db01e649500f5a9099
hlink27 <hblado@protonmail.ch>
2022-05-06 18:48:09 -0300

    Primeiro Teste

--- app/src/main/java/com/teste/estudo/MainActivity.java
+++ app/src/main/java/com/teste/estudo/MainActivity.java
@@ -2,14 +2,19 @@ package com.teste.estudo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText username, password;
+    Button btnLogin;
+    TextView crtAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,29 +22,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); //Disable Landscape Mode
 
-        EditText username, password;
-        Button btnLogin;
-
         //Find screen views
-        username = (EditText)findViewById(R.id.username);
-        password = (EditText)findViewById(R.id.password);
-        btnLogin = (Button)findViewById(R.id.btnLogIn);
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        crtAccount = (TextView) findViewById(R.id.txtCreateAccount);
+        btnLogin = (Button) findViewById(R.id.btnLogIn);
 
         //Login Button
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String loginUsername = username.getText().toString();
-                String loginPassword = password.getText().toString();
-                if(loginUsername.equals("admin") && loginPassword.equals("admin")){
-                    Toast.makeText(MainActivity.this, "passou", Toast.LENGTH_SHORT).show();
+                boolean check = checkInput();
+                if (check) {
+                    String loginUsername = username.getText().toString();
+                    String loginPassword = password.getText().toString();
+                    if (loginUsername.equals("admin") && loginPassword.equals("admin")) {
+                        Toast.makeText(MainActivity.this, getText(R.string.successLogIn) + loginUsername, Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MainActivity.this, MainMenu.class);
+                        startActivity(intent);
+                    } else {
+                        Toast.makeText(MainActivity.this, getText(R.string.incorrectInput), Toast.LENGTH_SHORT).show();
+                    }
                 } else {
-                    Toast.makeText(MainActivity.this, "não passou", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, getText(R.string.noLogInInput), Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
+        crtAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CreateAccount.class);
+                startActivity(intent);
+            }
+        });
+    }
 
-
+    //Validation
+    private boolean checkInput() {
+        if (username.length() < 3) {
+            username.setError(getText(R.string.errorInputLogin));
+            return false;
+        }
+        if (password.length() < 3) {
+            password.setError(getText(R.string.errorPasswordLogin));
+            return false;
+        } else {
+            return true;
+        }
     }
 }
