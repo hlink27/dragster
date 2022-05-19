d66304b5e08ffe73460f2baa8d6f50bd5fdf202f
hlink27 <hblado@protonmail.ch>
2022-05-19 19:19:28 -0300

    1905_3

--- app/src/main/java/com/teste/estudo/ScheduleActivity.java
+++ app/src/main/java/com/teste/estudo/ScheduleActivity.java
@@ -8,10 +8,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 public class ScheduleActivity extends AppCompatActivity {
+
+    EditText editTextDate, editTextTime;
+    TextInputEditText textDescricao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +25,9 @@ public class ScheduleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_schedule);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT); //Disable Landscape Mode
 
+        editTextDate = findViewById(R.id.editTextDate);
+        editTextTime = findViewById(R.id.editTextTime);
+        textDescricao = findViewById(R.id.textInput);
 
     }
 }
